46b1e66e46cfd3a6e5f987baf191c29d8219e56a
Josh Williams <josh.t.williams@outlook.com>
2021-11-24 02:10:13 -0800

    adding new functions

--- utils/storage.go
+++ utils/storage.go
@@ -1,13 +1,17 @@
 package utils
 
 import (
+	"archive/zip"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 var write_validation_1 = regexp.MustCompile(`^(\/[a-zA-Z0-9\-\_]{1,20})+$`)
@@ -110,4 +114,211 @@ func Write(filepath string, filename string, data string, mode int) error {
 
 	return nil
 
+}
+
+//Read contents of a file
+func ReadCsv(filename string) [][]string {
+
+	/*
+		Input:
+			(filename) string - Path of file to read
+		Output:
+			map[string]interface{} - JSON structured output
+	*/
+
+	response := fmt.Sprintf(`[ Function: ReadCsv ] [ Directory: %v ] [ Status: Success ]`, filename)
+
+	output := [][]string{}
+
+	csv_file, _ := os.Open(filename)
+	r := csv.NewReader(csv_file)
+	record, err := r.ReadAll()
+	if err != nil {
+		response = fmt.Sprintf(`[ Function: ReadCsv ] [ Directory: %v ] [ Status: Failed ] [ Error: %v ]`, filename, err)
+		log.Println(response)
+		return output
+	}
+	output = record
+
+	return output
+
+}
+
+//Write contents of a file
+func WriteCsv(filepath string, filename string, data [][]string, mode int) error {
+
+	/*
+		Input:
+			(filename) string - Path of file to read
+		Output:
+			map[string]interface{} - JSON structured output
+	*/
+
+	response := fmt.Sprintf(`[ Function: WriteCsv ] [ Directory: %v ] [ File: %v ] [ Status: Success ]`, filepath, filename)
+
+	_, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		os.MkdirAll(filepath, os.FileMode(mode))
+	}
+
+	path := fmt.Sprintf("%v/%v", filepath, filename)
+
+	f, err := os.Create(path)
+	if err != nil {
+		log.Println(err)
+	}
+
+	writer := csv.NewWriter(f)
+
+	err = writer.WriteAll(data)
+
+	if err != nil {
+		response = fmt.Sprintf(`[ Function: WriteCsv ] [ Directory: %v ] [ File: %v ] [ Status: Failed ] [ Error: %v ]`, filepath, filename, err)
+		log.Println(response)
+		return err
+	}
+
+	log.Println(response)
+
+	return nil
+
+}
+
+func Unzip(src, dest string) error {
+
+	response := fmt.Sprintf(`[ Function: Unzip ] [ Source: %v ] [ Destination: %v ] [ Status: Success ]`, src, dest)
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip (Directory traversal)
+		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", path)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			response = fmt.Sprintf(`[ Function: Unzip ] [ Source: %v ] [ Destination: %v ] [ Status: Failed ] [ Error: %v ]`, src, dest, err)
+			log.Println(response)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func Zip(src, dest string) error {
+
+	response := fmt.Sprintf(`[ Function: Zip ] [ Source: %v ] [ Destination: %v ] [ Status: Success ]`, src, dest)
+
+	// Get a Buffer to Write To
+	outFile, err := os.Create(dest)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer outFile.Close()
+
+	// Create a new zip archive.
+	w := zip.NewWriter(outFile)
+
+	// Add some files to the archive.
+	addFiles(w, src, "")
+
+	if err != nil {
+		response = fmt.Sprintf(`[ Function: Zip ] [ Source: %v ] [ Destination: %v ] [ Status: Failed ] [ Error: %v ]`, src, dest, err)
+		log.Println(response)
+		return err
+	}
+
+	// Make sure to check the error on Close.
+	err = w.Close()
+	if err != nil {
+		response = fmt.Sprintf(`[ Function: Zip ] [ Source: %v ] [ Destination: %v ] [ Status: Failed ] [ Error: %v ]`, src, dest, err)
+		log.Println(response)
+		return err
+	}
+
+	return nil
+}
+
+func addFiles(w *zip.Writer, basePath, baseInZip string) {
+	// Open the Directory
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, file := range files {
+		// fmt.Println(basePath + file.Name())
+		if !file.IsDir() {
+			dat, err := ioutil.ReadFile(basePath + "/" + file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// Add some files to the archive.
+			f, err := w.Create(baseInZip + file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+			_, err = f.Write(dat)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if file.IsDir() {
+
+			// Recurse
+			newBase := basePath + "/" + file.Name() + "/"
+			fmt.Println("Recursing and Adding SubDir: " + file.Name())
+			fmt.Println("Recursing and Adding SubDir: " + newBase)
+
+			addFiles(w, newBase, baseInZip+file.Name()+"/")
+		}
+	}
 }
